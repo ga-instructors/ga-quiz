083a49db5fe89dfc914184349e08d724bd1ab4d4
Jaden Carver <jaden.carver@gmail.com>
2015-03-17 23:32:40 -0400

    Converting application.js to CoffeeScript and removing notices in individual views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
 
-  def notice
-    content_tag(:div, super, class: 'alert-box row', data: { alert: true }) if super
-  end
-
   def gravatar_url(email, size: 48, default: 'retro')
     gravatar_id = Digest::MD5.hexdigest(email.downcase)
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{default}"
