2e875a11b6993956203274ba06f4734a7d919734
Jaden Carver <jaden.carver@gmail.com>
2015-04-22 14:57:50 -0700

    fixed dashboard for users who have not logged in before

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,6 +1,7 @@
 class Session < ActiveRecord::Base
   belongs_to :user
   attr_accessor :email, :password
+  after_create :clear_old_sessions
 
   def request=(request)
     self.user_agent = request.user_agent
@@ -19,6 +20,12 @@ class Session < ActiveRecord::Base
     else
       errors.add(:user, 'is invalid')
     end
+  end
+
+  private
+
+  def clear_old_sessions
+    user.sessions.where('created_at < ?', 3.month.ago).destroy_all if user.sessions.count > 10
   end
 
 end
