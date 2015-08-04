0a101083aa9498601ab759be5e99d758661e28f5
Jaden Carver <jaden.carver@gmail.com>
2015-08-03 23:09:54 -0400

    Destroying Groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
-  has_many :quizzes, class_name: 'Quizzes::Quiz'
-  has_many :group_members
+  has_many :quizzes, class_name: 'Quizzes::Quiz', dependent: :destroy
+  has_many :group_members, dependent: :destroy
   accepts_nested_attributes_for :group_members, allow_destroy: true
   has_many :users, through: :group_members
 
@@ -8,7 +8,7 @@ class Group < ActiveRecord::Base
   has_many :students, -> { where( group_members: { role: 'student' } ) }, through: :group_members, source: :user
   has_many :members, -> { where( group_members: { role: 'member' } ) }, through: :group_members, source: :user
 
-  has_many :regroups, class_name: 'Groups::Regroup'
+  has_many :regroups, class_name: 'Groups::Regroup', dependent: :destroy
 
   scope :active, -> { where('(groups.start_at IS NULL OR NOW() > groups.start_at) AND (groups.end_at IS NULL OR NOW() < groups.end_at)') }
 
